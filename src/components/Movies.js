f2f187e5b8e6a973efd51ac3f470d5c2e0645516
Chris Armitage <dev@chrisarmitage.com>
2023-01-10 15:37:47 +0000

    Call backend API for movies list

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,26 +5,18 @@ const Movies = () => {
     const [ movies, setMovies] = useState([])
 
     useEffect(() => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "Highlander",
-                release_date: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Description of film",
-            },
-            {
-                id: 2,
-                title: "Raiders of the Lost Ark",
-                release_date: "1981-06-12",
-                runtime: 115,
-                mpaa_rating: "PG-13",
-                description: "Description of film",
-            },
-        ]
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
 
-        setMovies(moviesList)
+        const rqOpts = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch(`http://localhost:8080/movies`, rqOpts)
+            .then(rs => rs.json())
+            .then(data => setMovies(data))
+            .catch(err => console.error(err))
     }, [])
 
     return (
