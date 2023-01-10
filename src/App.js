54b99d7e780240a4ecd38cb8082633a2fc8851ae
Chris Armitage <dev@chrisarmitage.com>
2023-01-10 13:40:56 +0000

    Add logic for displaying menu based on JWT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("")
+
+
   return (
     <div className="container">
       <div className="row">
@@ -8,9 +12,10 @@ function App() {
           <h1 className="mt-3">Go watch a movie</h1>
         </div>
         <div className="col text-end">
-          <Link to="/login">
-            <span className="badge bg-success">Login</span>
-          </Link>
+          {jwtToken === ""
+          ? <Link to="/login"><span className="badge bg-success">Login</span></Link>
+          : <Link to="#!"><span className="badge bg-danger">Logout</span></Link>
+          }
         </div>
         <hr className="mb-3" />
       </div>
@@ -22,9 +27,13 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
+              {jwtToken !== "" &&
+              <>
               <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
               <Link to="/manage-catalog" className="list-group-item list-group-item-action">Manage Catalog</Link>
               <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+              </>
+}
             </div>
           </nav>
         </div>
