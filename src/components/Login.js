44af25b589625aa2b550921e46305269780e8e92
Chris Armitage <dev@chrisarmitage.com>
2023-01-12 09:07:09 +0000

    Remove unused import

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { useNavigate, useOutletContext } from "react-router-dom"
-import App from "../App"
 import Input from "./form/Input"
 
 const Login = () => {
