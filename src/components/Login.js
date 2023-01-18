2ac3be8d577d1103a26fd68aa61de617bb5fc0d2
Chris Armitage <dev@chrisarmitage.com>
2023-01-18 17:05:32 +0000

    Call the authenticate endpoint

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,15 +17,39 @@ const Login = () => {
         event.preventDefault()
         console.log("[email, pass]", email, password)
 
-        if (email === "admin@example.com") {
-            setJwtToken("ey")
-            setAlertMessage("")
-            setAlertClassName("d-none")
-            navigate("/")
-        } else {
-            setAlertMessage("Invalid credentials")
-            setAlertClassName("alert-danger")
+        // build the request payload
+        let payload = {
+            email: email,
+            password: password,
         }
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            credentials: 'include',
+            body: JSON.stringify(payload)
+        }
+
+        fetch(`/authenticate`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    setAlertClassName("alert-danger")
+                    setAlertMessage(data.message)
+                } else {
+                    setJwtToken(data.access_token)
+                    setAlertClassName("d-none")
+                    setAlertMessage("")
+                    navigate("/")
+                }
+            })
+            .catch(error => {
+                setAlertClassName("alert-danger")
+                setAlertMessage(error.message)
+                console.log(error)
+            })
     }
 
     return (
