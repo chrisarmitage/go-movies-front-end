294cd098f59a172905f3f5dab138695e9033bb7e
Chris Armitage <dev@chrisarmitage.com>
2023-01-30 14:03:26 +0000

    Add All Genres page

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,12 +1,59 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
 const Genres = () => {
-    return (
-        <>
-            <div className="text-center">
-                <h2>Genres</h2>
-                <hr />
-            </div>
-        </>
-    )
+
+    const [ genres, setGenres ] = useState([]);
+    const [ error, setError ] = useState(null)
+
+    useEffect(() => {
+        const rqHeaders = new Headers()
+        rqHeaders.append("Content-Type", "application/json")
+
+        const rqOptions = {
+            method: "GET",
+            headers: rqHeaders,
+        }
+
+        fetch(`/genres`, rqOptions)
+            .then(rs => rs.json())
+            .then(data => {
+                if (data.error) {
+                    setError(data.message)
+                } else {
+                    setGenres(data)
+                }
+            })
+            .catch(err => { console.log(err) })
+    })
+
+    if (error !== null) {
+        return <div>Error: {error.message}</div>
+    } else {
+        return (
+            <>
+                <div>
+                    <h2>Genres</h2>
+                    <hr />
+
+                    <div className="list-group">
+                        {genres.map(g => (
+                            <Link
+                                key={g.id}
+                                className="list-group-item list-group-item-action"
+                                to={`/genres/${g.id}`}
+                                state={
+                                    { genreName: g.genre, } // pass a prop via the react router
+                                }
+                            >
+                                {g.genre}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default Genres
